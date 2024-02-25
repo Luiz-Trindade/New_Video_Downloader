47bdb88c4eaf6842a7bdf9468db4c7da4ef0448a
Luiz Trindade <149267777+Luiz-Trindade@users.noreply.github.com>
2024-02-25 17:40:18 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,56 @@
+<image url="https://www.gnu.org/graphics/gplv3-127x51.png">
+
+# Video Downloader
+
+## Simple YouTube Video Downloader Written In Python!
+
+This is a Python script for downloading YouTube videos. It provides a user-friendly interface built with Tkinter and utilizes the Pytube library for video downloading. The script is distributed under the GNU General Public License (GPL) version 3.
+
+### Usage:
+
+1. Run the script.
+2. In the "Download" tab, enter the URL of the video you want to download.
+3. Click the "Add" button to add the video to the download list.
+4. Optionally, you can reset the list using the "Reset" button.
+5. Choose the destination path by clicking the "Download" button.
+6. The download process will be divided into four parts to enhance efficiency.
+
+### Features:
+
+- **Multi-Processing:** The script uses multiprocessing to download multiple videos simultaneously, improving download speed.
+
+- **Notification:** Upon completion of the download, a notification sound is played, and a notification window appears.
+
+- **Dark Mode:** The script supports dark mode for a more comfortable experience.
+
+### Dependencies:
+
+- CustomTkinter: A customized version of Tkinter for enhanced aesthetics.
+  
+- Pytube: A library for downloading YouTube videos.
+
+- Pygame: Used for playing notification sounds.
+
+### How to Run:
+
+Make sure you have Python installed. Install the required libraries using:
+
+```bash
+pip install pytube customtkinter pygame
+```
+
+Run the script:
+
+```bash
+python your_script_name.py
+```
+
+### License:
+
+This program is free software under the terms of the GNU General Public License as published by the Free Software Foundation. See the [GPL-3.0 License](https://www.gnu.org/licenses/) for more details.
+
+### Author:
+
+- Luiz Gabriel Magalhães Trindade
+
+Feel free to contribute, report issues, or suggest improvements. Happy downloading! 🚀
---
+++ new_video_downloader.py
@@ -0,0 +1,136 @@
+gpl3_text='''
+    Simple YouTube Video Downloader Written In Python!
+
+    Copyright (C) 2024  Luiz Gabriel Magalhães Trindade.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+'''
+
+from customtkinter import *
+#from yt_dlp import YoutubeDL
+from pytube import YouTube
+from tkinter import PhotoImage, filedialog
+from time import sleep
+from multiprocessing import Process
+import pygame as pg
+pg.init()
+
+url_list = []
+alert_sound = ("_internal/sound/alert_sound.mp3")
+pg.mixer.music.load(alert_sound)
+
+def Alert(msg):
+    pg.mixer.music.play()
+    Alert = CTkToplevel(master=app)
+    Alert.title("Notification🔔")
+    Alert.geometry("450x150")
+    Alert.iconphoto(False, app_icon)
+    Alert_Message = CTkLabel(master=Alert, text=msg, font=("Arial", 30, "bold"))
+    Alert_Message.pack(pady=20, padx=10)
+    #sleep(5)
+
+def Add_Url_To_List():
+    global url_list
+    url = video_url_entrt.get()
+    if url:
+        url_list.append(url)
+        total_videos_label.configure(text=f"Total Videos: {len(url_list)}")
+        video_url_entrt.delete(0, len(url))
+    else: Alert("Add an URL!")
+
+def Download_Videos(part, path):
+    global url_list
+    
+    for i in part:
+        yt = YouTube(i)
+        video_stream = yt.streams.first()
+        video_stream.download(output_path=path)
+
+
+def Reset_List():
+    global url_list
+    url_list = []
+    total_videos_label.configure(text="Total Videos: 0")
+
+def Main():
+    global url_list
+
+    download_path = filedialog.askdirectory(title="Select the destination path")
+    qntd = int(int(len(url_list)) / 4)
+
+    part1 = url_list[:qntd]
+    part2 = url_list[qntd:qntd*2]
+    part3 = url_list[qntd*2:qntd*3]
+    part4 = url_list[qntd*3:]
+
+    p1 = Process(target=Download_Videos, args=(part1, download_path,))
+    p2 = Process(target=Download_Videos, args=(part2, download_path,))
+    p3 = Process(target=Download_Videos, args=(part3, download_path,))
+    p4 = Process(target=Download_Videos, args=(part4, download_path,))
+
+    p1.start()
+    p2.start()
+    p3.start()
+    p4.start()
+
+    p1.join()
+    p2.join()
+    p3.join()
+    p4.join() 
+
+    Alert("Download(s) concluído(s) com sucesso!")
+
+
+set_widget_scaling(1.5)
+set_appearance_mode("dark")
+set_default_color_theme("green")
+
+app = CTk()
+app.title("New Video Downloader")
+app.geometry("600x400")
+app_icon = PhotoImage(file="_internal/icons/icon.png")
+app.iconphoto(False, app_icon)
+
+tabview = CTkTabview(master=app)
+tabview.pack(pady=10, padx=10)
+
+tab1 = tabview.add("Download")
+tab2 = tabview.add("About")
+
+frame = CTkFrame(master=tab1)
+frame.pack(pady=20, padx=10)
+
+total_videos_label = CTkLabel(master=frame, text="Total Videos: 0", font=("Arial", 20, "bold"), justify="center")
+total_videos_label.grid(row=0, column=0, pady=10, padx=10)
+
+video_url_entrt = CTkEntry(master=frame, placeholder_text="Video URL", justify="center")
+video_url_entrt.grid(row=1, column=0, pady=10, padx=10)
+
+add_url_button = CTkButton(master=frame, text="Add", command=Add_Url_To_List)
+add_url_button.grid(row=1, column=1, pady=10, padx=10)
+
+reset_list_button = CTkButton(master=frame, text="Reset", fg_color="red", hover_color="orange",command=Reset_List)
+reset_list_button.grid(row=2, column=0, pady=10, padx=10)
+
+download_button = CTkButton(master=frame, text="Download", command=Main)
+download_button.grid(row=2, column=1, pady=10, padx=10)
+
+
+frame2 = CTkFrame(master=tab2)
+frame2.pack(pady=10, padx=10)
+
+about_label = CTkLabel(master=frame2, text=gpl3_text, font=("Arial", 8, "bold"), justify="left")
+about_label.pack(pady=10, padx=10)
+
+app.mainloop()
